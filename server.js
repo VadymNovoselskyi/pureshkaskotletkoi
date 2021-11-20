cbab75a408102465f9ce770c7e1f9dfa395a2c8e
VadymNovoselskyi <novoseslkyi.vadim@gmail.com>
2021-11-20 09:41:08 +0100

    test

--- server.js
+++ server.js
@@ -1,4 +1,31 @@
+// Load Node modules
 var express = require('express');
+const ejs = require('ejs');
+// Initialise Express
 var app = express();
+// Render static files
 app.use(express.static('public'));
-app.listen(8080);
+// Set the view engine to ejs
+app.set('view engine', 'ejs');
+// Port website will run on
+app.listen(process.env.PORT);
+// Route Route
+app.get('/', function (req, res) {
+    var name = "Louise";
+    var listnames = ["Louise", "Sadie", "Erik", "Ralph", "Gina"];
+    // Render index page
+    res.render('pages/index.ejs', {
+        // EJS variable and server-side variable
+        name, listnames
+    });
+});
+// Route Route
+app.get('/test', function (req, res) {
+    var name = "Louise";
+    var listnames = ["1", "2", "3", "4", "5"];
+    // Render index page
+    res.render('pages/index.ejs', {
+        // EJS variable and server-side variable
+        name, listnames
+    });
+});
